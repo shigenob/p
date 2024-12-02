c2f754ccbfca55586aadd2e6da21979ad59f1946
shigenob <24hs205@s.bgu.ac.jp>
2024-12-02 13:45:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,24 @@
                 result = "中吉";
             } else if (colour === "黒" && number === 2) {
                 result = "凶";
+                  } else if (colour === "金" && number === 3) {
+                result = "末吉";  
             } else if (colour === "白" && number === 0) {
                 result = "中吉";小吉
             } else if (colour === "白" && number === 1) {
                 result = "小吉";
             } else if (colour === "白" && number === 2) {
                 result = "大凶";
+                    } else if (colour === "金" && number === 3) {
+                result = "末吉";
             } else if (colour === "オレンジ" && number === 0) {
                 result = "凶";
             } else if (colour === "オレンジ" && number === 1) {
                 result = "大吉";
             } else if (colour === "オレンジ" && number === 2) {
                 result = "中吉";
-            }
+            } else if (colour === "黒" && number === 3) {
+                result = "末吉";
 
             document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
         }
