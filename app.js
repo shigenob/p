1a16c63d81ed7841f4c37b0bd6aa42a53adf4663
shigenob <24hs205@s.bgu.ac.jp>
2024-12-02 13:36:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
             var result = "凶"; // デフォルトの値
 
             if (colour === "黒" && number === 0) {
-                result = "小吉";
+                result = "末吉";
             } else if (colour === "黒" && number === 1) {
                 result = "中吉";
             } else if (colour === "黒" && number === 2) {
                 result = "凶";
             } else if (colour === "白" && number === 0) {
-                result = "中吉";
+                result = "中吉";小吉
             } else if (colour === "白" && number === 1) {
-                result = "中吉";
+                result = "小吉";
             } else if (colour === "白" && number === 2) {
                 result = "大凶";
             } else if (colour === "オレンジ" && number === 0) {
